832c66c5133214eaaec83fe69073df04ddbe4a5c
Randy <randy2vo@gmail.com>
2026-02-03 14:41:42 -0600

    Updated user.cpp line 25

--- user.cpp
+++ user.cpp
@@ -1,42 +1,60 @@
 #include <iostream>
-#include <cstdlib>
-#include <unistd.h>
+#include <string>
+#include <unistd.h>   // getpid, getppid, sleep
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-	if(argc != 2) {
-		cerr << "Usage: " << argv[0] << " iterations\n";
-		return 1;
-	}
+static int parseIterations(int argc, char* argv[]) {
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " iterations\n";
+        return -1;
+    }
 
-	int iterations = atoi(argv[1]);
-	
-	if (iterations < 0) {iterations = 0;
+    try {
+        size_t pos = 0;
+        int iters = stoi(argv[1], &pos);
 
-	}
+        // Reject junk like "10abc"
+        if (pos != string(argv[1]).size()) {
+            cerr << "Error: iterations must be an integer.\n";
+            return -1;
+        }
 
-	pid_t pid = getpid();
-	pid_t ppdit = getppid();
+        if (iters < 0) {
+            cerr << "Error: iterations must be >= 0.\n";
+            return -1;
+        }
 
-
-	for (int i = 1; i <= iterations; i++) {
-		cout << "USER PID:" << pid
-		     << " PPID: " << ppid
-		     << " Iteration: " << i
-		     << "before sleeping\n";
-
-		cout.flush();
-
-		sleep(1);
-
-		cout << "USER PID:" << pid
-                  << " PPID:" << ppid
-                  << " Iteration:" << i
-                  << " after sleeping\n";
-		cout.flush();
-	}
-
-	return 0;
-
+        return iters;
+    } catch (...) {
+        cerr << "Error: iterations must be an integer.\n";
+        return -1;
+    }
 }
+
+int main(int argc, char* argv[]) {
+    int iterations = parseIterations(argc, argv);
+    if (iterations < 0) return 1;
+
+    pid_t pid = getpid();
+    pid_t ppid = getppid();
+
+    for (int i = 1; i <= iterations; i++) {
+        cout << "USER PID:" << pid
+             << " PPID:" << ppid
+             << " Iteration:" << i
+             << " before sleeping\n";
+        cout.flush();
+
+        sleep(1);
+
+        cout << "USER PID:" << pid
+             << " PPID:" << ppid
+             << " Iteration:" << i
+             << " after sleeping\n";
+        cout.flush();
+    }
+
+    return 0;
+}
+
